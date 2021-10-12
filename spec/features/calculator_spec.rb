d7a34d817946f3bb763309ffd639d439eb2ac153
Kenneth John Balgos <kennethjohnbalgos@gmail.com>
2021-10-12 20:48:21 +0800

    Improve results display

--- spec/features/calculator_spec.rb
+++ spec/features/calculator_spec.rb
@@ -22,17 +22,17 @@ describe "Calculator", type: :feature do
   scenario "visitor can use the calculator field" do
     calculate("10 IMG 15 FLAC 13 VID")
 
-    expect(page).to have_text "10 IMG $800" # IMG output is displayed
-    expect(page).to have_text "15 FLAC $1957.50" # FLAC is displayed
-    expect(page).to have_text "13 VID $2370" # VID is displayed
+    expect(page).to have_text "10 IMG - $800.00" # IMG output is displayed
+    expect(page).to have_text "15 FLAC - $1,957.50" # FLAC is displayed
+    expect(page).to have_text "13 VID - $2,370.00" # VID is displayed
   end
 
   scenario "visitor can use lower case codes" do
     calculate("15 img 15 flac 15 vid")
 
-    expect(page).to have_text "15 IMG $1250" # IMG output is displayed
-    expect(page).to have_text "15 FLAC $1957.50" # FLAC is displayed
-    expect(page).to have_text "15 VID $2670" # VID is displayed
+    expect(page).to have_text "15 IMG - $1,250.00" # IMG output is displayed
+    expect(page).to have_text "15 FLAC - $1,957.50" # FLAC is displayed
+    expect(page).to have_text "15 VID - $2,670.00" # VID is displayed
   end
 
 end
